c79e910215ad642ec59de57e699efe4149ddc2b6
Felipe Hanzava <fhanzava@hotmail.com>
2021-08-31 15:59:03 -0300

    Initial commit

---
+++ app/src/main/java/one/digitalinnovation/applicationworkapi/api/ProductApi.kt
@@ -0,0 +1,10 @@
+package one.digitalinnovation.applicationworkapi.api
+
+import one.digitalinnovation.applicationworkapi.model.Product
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ProductApi {
+    @GET("getdata.php")
+    fun getProductApi(): Call<List<Product>>
+}
